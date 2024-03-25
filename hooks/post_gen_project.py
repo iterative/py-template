5ca1386b1e1432e7039647286f6bf8dcce1f07b2
skshetry <18718008+skshetry@users.noreply.github.com>
2024-03-25 12:49:55 +0545

    fix ci (#124)
    * fix ci
    
    * update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,7 @@ import shutil
 from pathlib import Path
 
 
-if not {{ cookiecutter.docs }}:
+if not {{cookiecutter.docs}}:  # noqa: F821
     shutil.rmtree("docs")
     (Path(".github") / "workflows" / "docs.yml").unlink()
     Path("mkdocs.yml").unlink()
